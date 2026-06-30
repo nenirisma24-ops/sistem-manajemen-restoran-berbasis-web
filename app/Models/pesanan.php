60dcf217e0237de7d5fc4e83c2ddab8f74f7c48a
alfiaantika.240604029@student.hamzanwadi.ac.id <alfiaantika.240604029@student.hamzanwadi.ac.id>
2026-06-30 13:18:28 +0800

    update

--- app/Models/pesanan.php
+++ app/Models/pesanan.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pesanan extends Model
 {
@@ -14,7 +15,7 @@ class Pesanan extends Model
         'total_price'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
